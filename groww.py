eabbdb2338dfd0076688a05e015ac582c3e3a23b
VedhaSankar <vedhasankar26@gmail.com>
2022-08-25 09:32:09 +0530

    got soup for required stock

--- groww.py
+++ groww.py
@@ -1,5 +1,6 @@
 # https://groww.in/
 
+from tkinter import CURRENT
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -7,39 +8,66 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import ActionChains
 import time 
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
+import requests
 
 PATH = "/home/vedha/softwares/chromedriver"
-# driver = webdriver.Chrome(service)
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+chrome_options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+chrome_options.add_argument(f'user-agent={userAgent}')
+chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 action = ActionChains(driver)
 
+def get_stock_url(stock):
 
-
-def startpy():
     driver.get("https://groww.in/search")
 
     # gets first element from the given tag
     # search = driver.find_element_by_name("q")
-    stock = driver.find_element(By.CSS_SELECTOR, '#searchPage > div.web-align > div.sp23SearchBox.absolute-center.clrText130 > div.sp23EntitySelect.fs15 > div > div > div > div > div.pos-rel.valign-wrapper.se55SelectBox.clrText > input')
-    stock = stock.send_keys(Keys.ARROW_DOWN)
+
+    stock_selection = driver.find_element(By.CSS_SELECTOR, '#searchPage > div.web-align > div.sp23SearchBox.absolute-center.clrText130 > div.sp23EntitySelect.fs15 > div > div > div > div > div.pos-rel.valign-wrapper.se55SelectBox.clrText > input')
+    stock_selection = stock_selection.send_keys(Keys.ARROW_DOWN)
+
     search = driver.find_element(By.XPATH, '//*[@id="sp23Input"]')
-    print(search)
     search.click()
-    # sm = stock.find_element(By.XPATH, '//*[@id="searchPage"]/div[2]/div[1]/div[1]/div').click()
-    search.send_keys("INFY", Keys.ENTER)
+    search.send_keys(stock, Keys.ENTER)
 
     time.sleep(5)
-
 
     reqd_stock = search.find_element(By.XPATH, '//*[@id="searchPage"]/div[2]/div[2]/div[2]/div/div[1]').click()
 
-    # search = driver.find_element(By.CSS_SELECTOR, "#globalSearch23-suggestionsContainer > div.se28GetDivAboveSuggestion > div > div:nth-child(2)")
-    
+    cur_url = driver.current_url
 
-    # search.send_keys(Keys.RETURN)
     time.sleep(5)
+    driver.quit()
 
-    # driver.quit()
+    return cur_url
+
+
+def scrap_stock_url(stock):
+
+    cur_url = get_stock_url(stock)
+
+    url_text = requests.get(cur_url).text
+
+    soup = BeautifulSoup(url_text, 'lxml') 
+
+    print(soup)
+
+
+def startpy():
+
+    scrap_stock_url('INFY')
+
+
 
 
 if __name__ == '__main__':
