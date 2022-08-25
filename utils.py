ff0ecd1ae2aafcce991bfa553bdae1666e643995
varshacb <83687662+varshacb@users.noreply.github.com>
2022-08-25 16:47:16 +0530

    stocks low value obtained

--- utils.py
+++ utils.py
@@ -1,40 +1,33 @@
+#from numpy import _2Tuple
+import numpy
 import yfinance as yf
 
+TICKER_LIST = ['TSLA','AMZN','AAPL','w','AMD']
 #define the ticker symbol
-tickerSymbol = "AAPL"
+def get_low_value(tickerSymbol):
 
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
+    # tickerSymbol = "AAPL"
 
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2022-08-23', end='2022-08-23')
+    #get data on this ticker
+    tickerData = yf.Ticker(tickerSymbol)
 
-#see your data
-# print(tickerDf.tail())
-for row in tickerDf.iterrows():
-    print (row[2])
-
-# import pandas as pd
-# import requests
-# from dotenv import load_dotenv
-# import os
-
-# load_dotenv()
-
-# API_KEY = os.environ.get('API_KEY')
+    #get the historical prices for this ticker
+    tickerDf = tickerData.history(period='1d', start='2022-08-23', end='2022-08-23')
+        
 
 
-# def get_intraday_data(symbol, interval):
-#     # API_KEY = open(r'api_key.txt')
-#     api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}&apikey={API_KEY}'
-#     raw_df = requests.get(api_url).json()
-#     df = pd.DataFrame(raw_df[f'Time Series ({interval})']).T
-#     df = df.rename(columns = {'1. open': 'open', '2. high': 'high', '3. low': 'low', '4. close': 'close', '5. volume': 'volume'})
-#     for i in df.columns:
-#         df[i] = df[i].astype(float)
-#     df.index = pd.to_datetime(df.index)
-#     df = df.iloc[::-1]
-#     return df
 
-# tsla_intra = get_intraday_data('NSI:WIPRO', '1min')
-# print(tsla_intra.tail())
+    # Use getitem ([]) to iterate over columns in pandas DataFrame
+    #for column in tickerDf:
+        #print(tickerDf[column].values)
+    l=tickerDf['Low'].values[0]
+    return l
+
+def get_all_low_values():
+
+    for ticker in TICKER_LIST:
+        print(get_low_value(ticker))
+    
+    pass
+if __name__ =='__main__':
+     get_all_low_values()
