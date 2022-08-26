84926e0c1fb52ae97457d5ed50324152c09b0021
VedhaSankar <vedhasankar26@gmail.com>
2022-08-26 09:34:42 +0530

    fixed merge changed

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import os
 from pymongo import MongoClient
+from datetime import datetime
 
 load_dotenv()
 
@@ -17,15 +18,47 @@ database = client[DB_NAME]
 app = Flask(__name__)
 
 
+# @app.route('/')
+# def start():
+
+#     return render_template('index.html')
+
 @app.route('/')
-def start():
-
-    return render_template('index.html')
-
-@app.route('/home')
 def home():
 
-    return render_template('home.html')
+    return render_template('home2.html')
+
+# @app.route('/signup', methods = ['GET', 'POST'])
+# def signup():
+
+#     if request.method == 'POST':
+
+#         aitceid         = request.values.get('aitceid')
+#         uname           = request.values.get('uname')
+#         college_name    = request.values.get('college_name')
+#         email_id        = request.values.get('email_id')
+#         password        = request.values.get('password')
+
+#         collection_name = 'user_details'
+
+#         current_user_id = get_prev_id(collection_name) + 1
+
+#         user_dict = {
+#             '_id'           : current_user_id,
+#             "aitceid"       : aitceid,
+#             "uname"         : uname,
+#             "college_name"  : college_name,
+#             "email_id"      : email_id,
+#             "password"      : password
+#         }
+
+
+#         new_collection = database[collection_name]
+#         x = new_collection.insert_one(user_dict)
+#         print(x)
+
+#     return render_template('sign_up.html')
+
 
 
 @app.route('/categories/', methods = ['GET', 'POST'])
@@ -41,22 +74,31 @@ def categories(category):
 @app.route('/new-product', methods=['GET', 'POST'])
 def new_product():
 
+    now = datetime.now()
+
     if request.method == 'POST':
-        c_name    = request.values.get("c_name")
-        p_name    = request.values.get("p_name")
-        price    = request.values.get("price")
-        inventory    = request.values.get("inventory")
-        categories    = request.values.get("categories")
-        description    = request.values.get("description")
+
+        c_name      = request.values.get("c_name")
+        p_name      = request.values.get("p_name")
+        price       = request.values.get("price")
+        inventory   = request.values.get("inventory")
+        categories  = request.values.get("categories")
+        man_site    = request.values.get("man_site")
+        description = request.values.get("description")
+        
+        dt_string   = now.strftime("%d/%m/%Y %H:%M:%S")
+
 
 
         result = {
-            'Company name'  : c_name,
-            'Product name'  : p_name,
-            'Price'         : price,
-            'inventory'     : inventory,
-            'categories'    : categories,
-            'description'   : description,
+            'Company name'          : c_name,
+            'Product name'          : p_name,
+            'Price'                 : price,
+            'Inventory'             : inventory,
+            'Categories'            : categories,
+            'Manufacturing site'    : man_site,
+            'Description'           : description,
+            'Time'                  : dt_string
         }
 
         collection_name = 'products'
